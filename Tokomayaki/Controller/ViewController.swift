dbcdf061efd2355ca866a2d71499428b31732375
solankidivyam <solankidivyam00@gmail.com>
2021-06-18 11:23:03 +0530

    New changes

--- Tokomayaki/Controller/ViewController.swift
+++ Tokomayaki/Controller/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let gradient = CAGradientLayer()
+        
+        gradient.colors = [UIColor.init(hex: "cc2b5e")?.cgColor, UIColor.init(hex: "753a88")?.cgColor]
+        let startPoint = CGPoint(x: 0.8, y: 0.5)
     }
 
 
---
+++ Tokomayaki/Model/Register.swift
@@ -0,0 +1,12 @@
+//
+//  Register.swift
+//  Tokomayaki
+//
+//  Created by Divyam Solanki on 17/06/21.
+//
+
+import Foundation
+
+struct Register {
+    
+}
