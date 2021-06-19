fccce09d28207682f627e8b28c17172dc43847ac
solankidivyam <solankidivyam00@gmail.com>
2021-06-19 13:38:58 +0530

    Fully complete login screen UI

--- Tokomayaki/Controller/LoginController.swift
+++ Tokomayaki/Controller/LoginController.swift
@@ -19,6 +19,28 @@ class LoginController: UIViewController, UIImagePickerControllerDelegate, UINavi
         
     }()
     
+    private let button1: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.9490196078, blue: 0.768627451, alpha: 1)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .light)
+        button.setTitle("Log in", for: .normal)
+        button.layer.cornerRadius = 12
+        button.setTitleColor(#colorLiteral(red: 0.3960784314, green: 0.3960784314, blue: 0.3960784314, alpha: 1), for: UIControl.State.normal)
+        return button
+        
+    }()
+    
+    private let button2: UIButton = {
+        let button = UIButton()
+//        button.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.9490196078, blue: 0.768627451, alpha: 1)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .light)
+        button.setTitle("Sign Up?", for: .normal)
+        button.layer.cornerRadius = 12
+        button.setTitleColor(#colorLiteral(red: 0.9559958577, green: 0.9549930692, blue: 0.8105223179, alpha: 1), for: UIControl.State.normal)
+        return button
+        
+    }()
+    
     //An Image view for the Background
     private let imageView1: UIImageView = {
     
@@ -50,16 +72,54 @@ class LoginController: UIViewController, UIImagePickerControllerDelegate, UINavi
         return label
     }()
     
+    private let label1: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont(name: "Avenir", size: 18)
+        label.textColor = UIColor.white
+        label.text = "or"
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let label2: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont(name: "Avenir", size: 10)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.text = "Create and account to be a part of Toko family, we deliver the best in class ice-creams across Japan and Taiwan. Starting soon in USA, Canada and UK..."
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
     private let textField1: UITextField = {
        
         let textField = UITextField()
 //        textField.layer.cornerRadius = 15.0
-        textField.font = UIFont(name: "Avenir", size: 15)
+        textField.font = UIFont(name: "Avenir Next", size: 15)
 //        textField.borderStyle = UITextField.BorderStyle.roundedRect
         textField.setCorner(radius: 12)
         textField.setLeftPaddingPoints(15)
         textField.backgroundColor = UIColor.white
-        textField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.5568627451, green: 0.5568627451, blue: 0.5568627451, alpha: 1)])
+        textField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.7254901961, green: 0.7254901961, blue: 0.7254901961, alpha: 1)])
+        
+        textField.keyboardType = UIKeyboardType.default
+        return textField
+    }()
+    
+    private let textField2: UITextField = {
+       
+        let textField = UITextField()
+//        textField.layer.cornerRadius = 15.0
+        textField.font = UIFont(name: "Avenir Next", size: 15)
+//        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.setCorner(radius: 12)
+        textField.setLeftPaddingPoints(15)
+        textField.backgroundColor = UIColor.white
+        textField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.7254901961, green: 0.7254901961, blue: 0.7254901961, alpha: 1)])
         
         textField.keyboardType = UIKeyboardType.default
         return textField
@@ -79,17 +139,31 @@ class LoginController: UIViewController, UIImagePickerControllerDelegate, UINavi
         view.addSubview(imageView1)
         view.addSubview(imageView)
         view.addSubview(textField1)
+        view.addSubview(textField2)
         view.addSubview(label)
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(button1)
+        view.addSubview(button2)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
    
         
-        textField1.frame = CGRect(x: view.frame.midX/3 + 30, y: view.safeAreaInsets.top + 220, width: 220, height: 37)
-            
+        button1.frame = CGRect(x: view.frame.size.width/3 + 10, y: view.safeAreaInsets.top + 330, width: 120, height: 37)
         
+        button2.frame = CGRect(x: view.frame.size.width/3 + 20, y: view.safeAreaInsets.top + 400, width: 100, height: 37)
+        
+        textField1.frame = CGRect(x: view.frame.midX/3 + 30, y: view.safeAreaInsets.top + 220, width: 220, height: 37)
+        
+        textField2.frame = CGRect(x: view.frame.midX/3 + 30, y: view.safeAreaInsets.top + 270, width: 220, height: 37)
+            
         label.frame = CGRect(x: view.frame.midX/3 + 15, y: view.safeAreaInsets.top + 130, width: 100, height: 100)
+        
+        label1.frame = CGRect(x: view.frame.size.width/3 + 45, y: view.safeAreaInsets.top + 365, width: 50, height: 50)
+        
+        label2.frame = CGRect(x: view.frame.size.width/3 - 52, y: view.safeAreaInsets.top + 420, width: 240, height: 100)
         
         imageView.frame = CGRect(x: view.frame.midX/3, y: view.safeAreaInsets.top - 50, width: view.frame.size.width-150, height: view.frame.size.width - 150)
         
